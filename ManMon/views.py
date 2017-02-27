00bf40fe5fd9afcb8a799f8f34e73bbe4423810b
FoXIsMaName <s5801012630050@email.kmutnb.ac.th>
2017-02-27 17:29:06 +0700

    delete unnecessary file

--- ManMon/views.py
+++ ManMon/views.py
@@ -46,7 +46,6 @@ def saveAccount(request, choice):
 
     ac = Account(note = note, money = money, type_note = type_note, pub_date = date)
     ac.save()
-
     return render(request, 'ManMon/saveAccount.html',{'note':note, 'money':money, 'type_note':type_note, 'date':date})
    
 def history(request):
@@ -55,89 +54,6 @@ def history(request):
     for account in account_list :
         money_sum += account.money
     return render(request, 'ManMon/history.html', {'account_list':account_list, 'money_sum':money_sum})
-
-#show payment information
-def tableIncome(request):
-    income_list = Income.objects.order_by('-pub_date')
-    income_sum = 0
-    for income in income_list :
-        income_sum += income.earning
-    return render(request, 'ManMon/showIncome.html', {'income_list':income_list,'incomesum':income_sum})
-
-#show payment information
-def tablePayment(request):
-    payment_list = Payment.objects.order_by('-pub_date')
-    payment_sum = 0
-    for payment in payment_list :
-        payment_sum += payment.buy_thing
-    return render(request, 'ManMon/showPayment.html', {'payment_list':payment_list, 'paymentsum':payment_sum})
-
-def callInsertIncome(request):
-    type_in_list = TypeIncome.objects.order_by('-type_income')
-    return render(request,"ManMon/insertIncome.html", {'type_in_list':type_in_list})
-
-#save income information
-def saveIncome(request):
-    income_list = Income.objects.order_by('-pub_date')
-    income_sum = 0
-    for income in income_list :
-        income_sum += income.earning
-
-    try:
-        note_income = request.POST['note_income']
-        money = request.POST['money']
-        date_income = request.POST['dmy_income']
-        type_income = request.POST['type_income']
-    except:
-        note_income = ""
-        money = ""
-        date_income = ""
-        type_income = ""
-    else:
-            IN = Income(income_text=note_income, earning=money, type_income = type_income, pub_date=date_income)
-            IN.save()
-    return render(request,"ManMon/showIncome.html",{'income_list':income_list,'incomesum':income_sum})
-    
-def callDelIncome(request):
-    income_list = Income.objects.order_by('-pub_date')
-    return render(request, 'ManMon/delIncome.html', {'income_list':income_list})
-
-def delIncome(request, income_id):
-    income = get_object_or_404(Income, pk=income_id)
-    income.delete()    
-    return render(request, "ManMon/main.html", '')
-
-def callInsertPayment(request):
-    type_pay_list = TypePayment.objects.order_by('-type_payment')    
-    return render(request, "ManMon/insertPayment.html",{"type_pay_list":type_pay_list})
-
-#save income information
-def savePayment(request):
-    payment_list = Payment.objects.order_by('-pub_date')
-    payment_sum = 0
-    for payment in payment_list :
-        payment_sum += payment.buy_thing
-
-    try:
-        note_payment = request.POST['note_payment']
-        payment = request.POST['payment']
-        date_payment = request.POST['dmy_payment']
-        type_payment = request.POST['type_payment']
-    except:
-        note_payment = ""
-        payment = ""
-        date_payment = ""
-        type_payment = ""
-    else:
-            OUT = Payment(payment_text=note_payment, buy_thing=payment, type_payment = type_payment, pub_date=date_payment)
-            OUT.save()
-
-    return render(request,"ManMon/showPayment.html",{'payment_list':payment_list, 'paymentsum':payment_sum})
-
-def delPayment(request, payment_id):
-    payment = Payment.objects.get(pk=payment_id)
-    payment.delete()
-    return render(request, "ManMon/delPayment.html",'')
 
 def callInsertType(request):
     return render(request,"ManMon/insertType.html",'')
